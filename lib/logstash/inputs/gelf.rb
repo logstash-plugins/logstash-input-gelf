a1ce2ef18a226145c3962d060f8619f0136e3169
mportermahoney <mportermahoney@wayfair.com>
2015-07-08 17:57:05 -0400

    do not remove timestamp from the message, this is a default field for the gelf format and should be there

--- lib/logstash/inputs/gelf.rb
+++ lib/logstash/inputs/gelf.rb
@@ -111,7 +111,6 @@ class LogStash::Inputs::Gelf < LogStash::Inputs::Base
       event["source_host"] = client[3]
       if event["timestamp"].is_a?(Numeric)
         event.timestamp = LogStash::Timestamp.at(event["timestamp"])
-        event.remove("timestamp")
       end
       remap_gelf(event) if @remap
       strip_leading_underscore(event) if @strip_leading_underscore
